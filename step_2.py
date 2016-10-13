bb661b5b089068718b1690c5771566f272bc8b26
Daniel Calderon Manriquez <calderon.daniel08@gmail.com>
2016-10-13 14:02:00 -0700

    Fixed a syntax error on line 20 that would prevent from getting the token

--- step_2.py
+++ step_2.py
@@ -5,7 +5,7 @@ dictionary = {'token': '2b911b9f12182ba5bf0b39cea4440605' }
 request = requests.post('http://challenge.code2040.org/api/reverse', json = dictionary)
 
 #utilized to make sure that my connection is correct and is retieving a string
-#print request.text
+print request.text
 
 string = request.text
 
@@ -16,7 +16,7 @@ size = len(string)
 for i in range(0,size):
     #works by addin the string to the new index EX reverse pizza; i + p
     string_reverse= string[i] + string_reverse
-#dictiona to json
-end = {'toke' : '2b911b9f12182ba5bf0b39cea4440605' , 'string' : string_reverse}
+#dictionary to json
+end = {'token' : '2b911b9f12182ba5bf0b39cea4440605' , 'string' : string_reverse}
 #json post request
 end_post = requests.post('http://challenge.code2040.org/api/reverse/validate', json = end)
